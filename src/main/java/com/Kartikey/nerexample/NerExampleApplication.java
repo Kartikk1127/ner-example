1ebc301c9c5e377cc3976bad9260c4d1e25d457b
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2024-03-20 07:50:28 +0530

    first commit

---
+++ src/main/java/com/Kartikey/nerexample/NerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.Kartikey.nerexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NerExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Kartikey/nerexample/NerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Kartikey.nerexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
